84024dfba1507b21fed70aeb20ae2a175e1b7393
Yuji Konishi <yuji.k64613@gmail.com>
2018-03-25 18:32:12 +0900

    db

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import os
+import psycopg2
 from bottle import route, run
 
 @route("/")
 def hello_world():
-    return "Hello World!";
+        DATABASE_URL = os.environ['DATABASE_URL']
+        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM mytable;")
+        data = cur.fetchone()
+        name1 = data[1]
+        return "Hello World2!" + str(name1);
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
